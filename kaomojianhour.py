381ad6c48831d22a5e45060a2db8badc8359edd5
anjaelisabeth <47307220+anjaelisabeth@users.noreply.github.com>
2019-02-12 17:07:30 -0600

    Update kaomojianhour.py
    new version attempts to scrape from wikipedia and writes scraped stuff to file used to write tweets. also hid api keys for security reasons, will use vnv to use keys when running

--- kaomojianhour.py
+++ kaomojianhour.py
@@ -1,18 +1,33 @@
 #this bot posts a kaomoji a day to @kaomojiaday
 
-
-import tweepy, time, wikipedia
+import tweepy, time, requests
 from bs4 import BeautifulSoup
 
 #made them into variables in case I need to regenerate keys 
 consumer_key = 'x7G0ZEg3wxXu2vc0fcHNM90z1'
-secret_consumer_key = 'Y30mnZvhOyKTFLXSLP9nsGGDfX7UyWWZoFT6HMPHI2wPsPVkBk'
-access_token = '1092225188329066496-u2xQz6V9z5gv7biCZ0LSulwMJGwg9m'
-secret_access_token = 'm72plBHoTrG8QZ2ChKQ6VRiuQZenRjSFyXNE3bHf54uRm'
+secret_consumer_key = 'CONSUMER KEY GOES HERE'
+access_token = 'ACCESS TOKEN GOES HERE'
+secret_access_token = 'SECRET ACCESS TOKEN GOES HERE'
 
 auth = tweepy.OAuthHandler(consumer_key, secret_consumer_key)
 auth.set_access_token(access_token, secret_access_token)
 myapi = tweepy.API(auth)
+
+wiki = "https://en.wikipedia.org/wiki/List_of_emoticons"
+soup = BeautifulSoup(requests.get(wiki).content, 'html.parser')
+table = soup.find('table', class_="wikitable")
+kaomoji = []
+for row in table.find_all('th', scope='row'):
+    kaomoji.append(row)
+#print(kaomoji)
+
+def add_kaomoji_to_file(kaomoji):
+    #add kaomoji scraped from wikipedia into file used to send tweets
+    file = open('kaomojilist.txt', 'r+', encoding='UTF-8')
+    for i in kaomoji:
+        file.write("%s\n" % i)
+    file.close()
+        
 
 def send_kaomoji_tweets():
     file = open('kaomojilist.txt', 'r+', encoding='UTF-8')
@@ -26,15 +41,12 @@ def follow_back():
     followers = myapi.followers()
     for follower in followers:
         follower.follow()
-    time.sleep(30)
     
-def main():
-    send_kaomoji_tweets()
-    while True:
-        follow_back()
-main()
+#def main():
+    #send_kaomoji_tweets()
+    #follow_back()
+
+#main()
 #myapi.update_profile_image('kaomojprofpic.jpg')
 
-    
-
-
+   
